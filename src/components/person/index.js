65838d4f4d9922797154ba5212ba6e5f95c1edd1
Matt Schroeder <matt.schroeder@objectpartners.com>
2018-08-10 10:07:58 -0500

    add linkedin icon to Person component (and make social media icons optional)

--- src/components/person/index.js
+++ src/components/person/index.js
@@ -1,23 +1,36 @@
-import React from 'react';
-import TwitterIcon from 'react-icons/lib/fa/twitter';
+import React from "react";
+import TwitterIcon from "react-icons/lib/fa/twitter";
+import LinkedinIcon from "react-icons/lib/fa/linkedin";
 
-import './person.css';
+import "./person.css";
 
-export default function Person({ avatar, name, title, twitter }) {
+export default function Person({ avatar, name, title, twitter, linkedin }) {
   return (
     <div className="person">
       <img className="person-avatar" src={avatar} />
       <div className="person-details">
         <h2 className="person-name">{name}</h2>
         <h3 className="person-title">{title}</h3>
-        <a
-          className="person-twitter"
-          href={`https://twitter.com/${twitter}`}
-          target="_blank"
-          rel="noopener"
-        >
-          <TwitterIcon className="icon" color="#1da1f2" />@{twitter}
-        </a>
+        {linkedin && (
+          <a
+            className="person-social"
+            href={`https://www.linkedin.com/in/${linkedin}`}
+            target="_blank"
+            rel="noopener"
+          >
+            <LinkedinIcon className="icon" color="#1da1f2" />@{linkedin}
+          </a>
+        )}
+        {twitter && (
+          <a
+            className="person-social"
+            href={`https://twitter.com/${twitter}`}
+            target="_blank"
+            rel="noopener"
+          >
+            <TwitterIcon className="icon" color="#1da1f2" />@{twitter}
+          </a>
+        )}
       </div>
     </div>
   );
